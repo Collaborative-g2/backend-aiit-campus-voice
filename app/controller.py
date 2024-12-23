4a8a7535556c45ef24db90f66838fab4fa6696e7
Ubuntu <ubuntu@ip-10-0-26-154.ap-northeast-1.compute.internal>
2024-12-23 15:36:58 +0000

    科目名検索機能を実装

--- app/controller.py
+++ app/controller.py
@@ -11,10 +11,27 @@ def health_handler(event, context):
     }
 
 def top_handler(event, context):
-    # DynamoDBから取得する代わりにモックデータを使用
     subjects = get_subjects()
     return {
         "statusCode": 200,
         "headers": {"Content-Type": "application/json"},
         "body": json.dumps(format_response(subjects))
     }
+
+def search_handler(event, context):
+   keyword = event.get('queryStringParameters', {}).get('q', '')
+   subjects = get_subjects()
+   
+   if keyword:
+       filtered_subjects = [
+           s for s in subjects 
+           if keyword in s['subject_name']
+       ]
+   else:
+       filtered_subjects = subjects
+
+   return {
+       "statusCode": 200,
+       "headers": {"Content-Type": "application/json"},
+       "body": json.dumps(format_response(filtered_subjects))
+   }
